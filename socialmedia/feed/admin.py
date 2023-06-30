7a3a6d066a1ad01e736fb180d69dde1e829eef47
mustafa <mustafabakrol@gmail.com>
2023-06-30 07:42:19 +0530

    updated email/username verification

--- socialmedia/feed/admin.py
+++ socialmedia/feed/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Post,PostComment,PostLike
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(PostLike)
+admin.site.register(PostComment)
--- socialmedia/usermanagement/urls.py
+++ socialmedia/usermanagement/urls.py
@@ -3,6 +3,6 @@ from .views import UserSignUpAPIView,UserLoginLogoutAPIView,UserDetailAPIView
 
 urlpatterns = [
     path('',UserSignUpAPIView.as_view()),
-    path('Auth/',UserLoginLogoutAPIView.as_view()),
+    path('auth/',UserLoginLogoutAPIView.as_view()),
     path('<int:pk>/',UserDetailAPIView.as_view())
 ]
